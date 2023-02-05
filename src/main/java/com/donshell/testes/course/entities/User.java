cc76455a411c580e7e04a47e619d1f2f362af6c6
DonShell <assum.dev@protonmail.com>
2023-02-05 13:27:28 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/donshell/testes/course/entities/User.java
+++ src/main/java/com/donshell/testes/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.donshell.testes.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -28,6 +31,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -95,6 +101,10 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 }
