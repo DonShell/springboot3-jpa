98856d72b08c7b4fab9b9d53622e2d5bd6ac3110
DonShell <assum.dev@protonmail.com>
2023-02-05 14:51:21 -0300

    OrderStatus enum

--- src/main/java/com/donshell/testes/course/config/testConfig.java
+++ src/main/java/com/donshell/testes/course/config/testConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.donshell.testes.course.entities.Order;
 import com.donshell.testes.course.entities.User;
+import com.donshell.testes.course.entities.enums.OrderStatus;
 import com.donshell.testes.course.repositories.OrderRepository;
 import com.donshell.testes.course.repositories.UserRepository;
 
@@ -37,12 +38,12 @@ public class testConfig implements CommandLineRunner {
 		
 
 		List<Order> orderList = new ArrayList<>();
-		orderList.add( new Order(null, Instant.parse("2019-06-20T19:53:07Z"), usersList.get(1)));
-		orderList.add( new Order(null, Instant.parse("2019-07-21T03:42:10Z"), usersList.get(2)));
-		orderList.add( new Order(null, Instant.parse("2019-07-22T15:21:22Z"), usersList.get(0)));
-		orderList.add( new Order(null, Instant.parse("2019-08-07T14:21:20Z"), usersList.get(1)));
-		orderList.add( new Order(null, Instant.parse("2019-08-07T14:25:22Z"), usersList.get(1)));
-		orderList.add( new Order(null, Instant.parse("2019-09-07T15:04:20Z"), usersList.get(3))); 
+		orderList.add( new Order(null, Instant.parse("2019-06-20T19:53:07Z"), usersList.get(1),OrderStatus.PAID));
+		orderList.add( new Order(null, Instant.parse("2019-07-21T03:42:10Z"), usersList.get(2),OrderStatus.CANCELED));
+		orderList.add( new Order(null, Instant.parse("2019-07-22T15:21:22Z"), usersList.get(0),OrderStatus.SHIPPED));
+		orderList.add( new Order(null, Instant.parse("2019-08-07T14:21:20Z"), usersList.get(1),OrderStatus.WAITING_PAYMENT));
+		orderList.add( new Order(null, Instant.parse("2019-08-07T14:25:22Z"), usersList.get(1),OrderStatus.DELIVERED));
+		orderList.add( new Order(null, Instant.parse("2019-09-07T15:04:20Z"), usersList.get(3),OrderStatus.CANCELED)); 
 
 		userRepository.saveAll(usersList);
 		orderRepository.saveAll(orderList);
--- src/main/java/com/donshell/testes/course/entities/Order.java
+++ src/main/java/com/donshell/testes/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.donshell.testes.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -33,12 +34,18 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	
+	private Integer orderStatus;
+	
+	
 	public Order() {}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, User client,OrderStatus orderStatus) {
+		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -63,6 +70,17 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null)
+		{
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
